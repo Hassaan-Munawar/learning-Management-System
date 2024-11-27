b9e0e652dafbae19aa5bf1b0ebc42e221cfb8832
Hassaan-Muanwar <mhassaanmunawarali@gmail.com>
2024-11-27 17:03:51 +0500

    code

--- src/actions/students.js
+++ src/actions/students.js
@@ -1,4 +1,6 @@
-"use server"
+"use server";
+
+import { revalidatePath } from "next/cache";
 
 export async function getStudents() {
     let users = await fetch(`${process.env.BASE_URL}/api/students`,{cache:"no-cache"});
