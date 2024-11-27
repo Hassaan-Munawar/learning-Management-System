90c4a5f04006de1695ea46804629997b8fb3ad57
Hassaan-Muanwar <mhassaanmunawarali@gmail.com>
2024-11-27 17:19:07 +0500

    code

--- src/actions/students.js
+++ src/actions/students.js
@@ -1,9 +1,13 @@
-"use server";
+// "use server";
 
-import { revalidatePath } from "next/cache";
+import { connectDB } from "@/lib/dbConnect";
+import { UserModal } from "@/lib/modals/UserModal";
+
 
 export async function getStudents() {
-    let users = await fetch(`${process.env.BASE_URL}/api/students`,{cache:"no-cache"});
-    users = await users.json();
+    // let users = await fetch(`${process.env.BASE_URL}/api/students`,{cache:"no-cache"});
+    // users = await users.json();
+    await connectDB();
+  const users = await UserModal.find();
     return users;
 }
