dd740e49b478d21bf4ba0605abadd79c845af296
Hassaan-Muanwar <mhassaanmunawarali@gmail.com>
2024-12-19 08:27:54 +0500

    code

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-        <div class="flex items-center justify-center min-h-[90vh] lg:min-h-[89vh] bg-blue-400">
+        <div class="flex items-center justify-center h-[90vh] lg:h-[89vh] bg-blue-400">
             <div class="loader border-t-4 border-b-4 border-white rounded-full w-44 h-44 animate-spin"></div>
         </div>
     )
